af57602687ab77369a1c421e1f4e2e05720adb8b
jnack <55568980+jnackmclain@users.noreply.github.com>
2023-02-18 11:27:20 -0600

    checksum harder xenia xex, back to patch arks

--- _build_xenia.bat
+++ _build_xenia.bat
@@ -1,12 +1,7 @@
 git pull https://github.com/hmxmilohax/dance-central-1-deluxe main
 @echo OFF
-echo:Make sure your 1.0 vanilla ark files are in _build/gen/
-del "%~dp0\_build\gen\main_xbox.hdr"
-del "%~dp0\_build\gen\main_xbox_7.ark"
-xcopy "%~dp0\dependencies\_rebuild_files\main_xbox.hdr" "%~dp0\_build\gen" >nul
 echo:Building Dance Central Deluxe patch arks.
-echo:The "Unhandled exception" below is expected, and does not indicate failure.
-"%~dp0dependencies/arkhelper" patchcreator -a "%~dp0\_ark" -o "%~dp0\_build" "%~dp0\_build\gen\main_xbox.hdr" >nul
+"%~dp0dependencies/arkhelper" dir2ark "%~dp0\_ark" "%~dp0\_build\gen" -n "patch_xbox" -e -v6 >nul
 echo:Wrote Dance Central Deluxe patch arks.
 echo:Complete. Launching Xenia. Enjoy Dance Central Deluxe
 "%~dp0\_xenia\xenia_canary" "%~dp0\_build\default_xenia.xex"
